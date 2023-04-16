853965f5208e0a17da65752d79f0d0606ddacf00
IvanVl94 <WlasowIwan@yandex.ru>
2023-04-16 13:41:51 +0300

    Новое задание

---
+++ src/main/java/pro/sky/JawaSkyPro/JawaSkyProApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.JawaSkyPro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JawaSkyProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JawaSkyProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/JawaSkyPro/JawaSkyProApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.JawaSkyPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JawaSkyProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
